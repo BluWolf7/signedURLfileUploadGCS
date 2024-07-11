b1c1d968a4459c8424e9fbdf84c0a026f6e65dc9
Arjun P <arj4c3@gmail.com>
2024-07-11 16:07:09 +0530

    initial commit

---
+++ src/main/java/com/signedUrlUploadGCS/signedUrlUploadGCS/SignedUrlUploadGcsApplication.java
@@ -0,0 +1,13 @@
+package com.signedUrlUploadGCS.signedUrlUploadGCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignedUrlUploadGcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignedUrlUploadGcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signedUrlUploadGCS/signedUrlUploadGCS/SignedUrlUploadGcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signedUrlUploadGCS.signedUrlUploadGCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignedUrlUploadGcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
